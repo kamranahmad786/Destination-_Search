83fb1354a82afce1baa5fb108ac2ae25d6638e59
kamran <mohammadkamranahmad786@gmail.com>
2024-03-21 09:05:04 +0000

    solution

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {imgUrl, name} = destinationDetails
+
+  return (
+    <li className="destination-item">
+      <img src={imgUrl} alt={name} className="destination-image" />
+      <p className="name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
